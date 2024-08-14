5a4f97eecc12c5cdb27b290c1e48d34d359794b0
Milana <msugic@gmail.com>
2024-08-14 13:46:22 +0200

    added dropdown menu functionality for mobile

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 window.addEventListener('load', loadJS);
 
 function loadJS() {
-    alert('window loaded');
+    // Variables
+    const mainLogoImage = document.getElementsByClassName('mk-main-nav__logo__image')[0];
+
+    const dropDownMenu = document.getElementsByClassName('mk-dropdown_menu')[0];
+    const dropDownMenuBtn = document.getElementById('mk-dropdown_menu-btn');
+
+    // Event listeners
+    dropDownMenuBtn.addEventListener('click', toggleMenu);
+
+    /* FUNCTIONS */
+    // toggle dropdown menu
+    function toggleMenu() {
+        // console.log(mainLogoImage);
+        mainLogoImage.classList.toggle('mk-is_active');
+        dropDownMenu.classList.toggle('mk-is_active');
+        dropDownMenuBtn.classList.toggle('mk-is_active');
+    }
+    
 }
